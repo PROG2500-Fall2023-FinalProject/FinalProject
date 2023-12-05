0ab6a188feb57043bad1742968ef04bc095ae040
alexsandria Ryan <alexsandriapatrice@gmail.com>
2023-12-04 20:38:56 -0400

    Inital setup with home page and main window

---
+++ FinalProject/FinalProject/MainWindow.xaml.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace FinalProject
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            mainFrame.NavigationService.Navigate(new Pages.Home());
+        }
+
+        private void Home_Click(object sender, RoutedEventArgs e)
+        {
+            mainFrame.NavigationService.Navigate(new Pages.Home());
+        }
+
+        private void ViewActors_Click(object sender, RoutedEventArgs e)
+        {
+            //mainFrame.NavigationService.Navigate(new Pages.Actors());
+        }
+
+        private void ViewGenres_Click(object sender, RoutedEventArgs e)
+        {
+            //mainFrame.NavigationService.Navigate(new Pages.Genres());
+        }
+
+        private void ViewMovies_Click(object sender, RoutedEventArgs e)
+        {
+            //mainFrame.NavigationService.Navigate(new Pages.Movies());
+        }
+
+        private void Exit(object sender, RoutedEventArgs e)
+        {
+            Close();
+        }
+    }
+}
